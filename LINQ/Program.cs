688d4f4a8c7f98e60f8f809fa11d74cf9b530ccd
Joseph LaVia <josephdlavia@gmail.com>
2023-05-31 10:44:28 -0500

    Linqq Exeercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videogames = new List<string>() { "Halo", "Fallout 3", "Call Of Duty", "Rainbow Six", "Grand Theft Auto", "Mortal Kombat", "Ghost Recon" };
+            var longNames = videogames.Where(videogames => videogames.Length > 10).ToList();
+            foreach (var videogams in longNames) 
+            {
+                Console.WriteLine(videogams);
+            }
         }
     }
 }
